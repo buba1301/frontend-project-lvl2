55c24bf706f319ff4801167d3ad6a7c14f4c3e40
Mona Lisa <bmuza57@gmail.com>
2020-02-19 21:54:06 +0400

    task1

---
+++ src/bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+// import { program } from 'commander';
+
+const program = require('commander');
+
+program
+  .version('1.1.1')
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format');
+program.parse(process.argv);
+
+if (program.format) console.log('%s', [program.format]);
