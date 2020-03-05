4b2b1132220d70e8f08b21ac9b87620c56a59e0b
Mona Lisa <bmuza57@gmail.com>
2020-03-05 12:29:13 +0300

    part6.1

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -5,6 +5,7 @@ describe('diffFile', () => {
   const pathToFixtures = path.join(__dirname, '__fixtures__');
 
   const actual = '{\n host: hexlet.io\n + verbose: true\n - timeout: 50\n + timeout: 20\n - proxy: 123.234.53.22\n - follow: false\n}';
+
   it('test json', () => {
     const filePathBefore = path.join(pathToFixtures, 'before.json');
     const filePathAfter = path.join(pathToFixtures, 'after.json');
@@ -15,6 +16,13 @@ describe('diffFile', () => {
   it('test yml', () => {
     const filePathBefore = path.join(pathToFixtures, 'before.yml');
     const filePathAfter = path.join(pathToFixtures, 'after.yml');
+    const diff = getDiff(filePathBefore, filePathAfter);
+    expect(diff).toBe(actual);
+  });
+
+  it('test ini', () => {
+    const filePathBefore = path.join(pathToFixtures, 'before.ini');
+    const filePathAfter = path.join(pathToFixtures, 'after.ini');
     const diff = getDiff(filePathBefore, filePathAfter);
     expect(diff).toBe(actual);
   });
--- src/parsers.js
+++ src/parsers.js
@@ -2,18 +2,17 @@ import yaml from 'js-yaml';
 import path from 'path';
 import fs from 'fs';
 import process from 'process';
+import ini from 'ini';
 
 const mapping = {
   yml: (data) => yaml.safeLoad(data),
   json: (data) => JSON.parse(data),
+  ini: (data) => ini.parse(data),
 };
 
 export default (filePath) => {
   const pathToFile = path.resolve(process.cwd(), filePath);
-  console.log(pathToFile);
   const type = path.extname(filePath).slice(1);
-  console.log(type);
   const data = fs.readFileSync(pathToFile).toString();
-  console.log(data);
   return mapping[type](data);
 };
