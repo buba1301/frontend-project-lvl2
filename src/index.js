86644f2bd6585a35e99e5f62c9971a2718ea6c98
Mona Lisa <bmuza57@gmail.com>
2020-03-05 11:40:21 +0300

    part 5.2

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ export default (filePath1, filePath2) => {
   const file1 = parser(filePath1);
   const file2 = parser(filePath2);
 
+  const beforeFileKeys = Object.keys(file1);
   const afterNewFileKeys = Object.keys(file2).filter((elem) => !beforeFileKeys.includes(elem));
   const addedItems = afterNewFileKeys.map((elem) => `+ ${elem}: ${file2[elem]}`);
 
-  const beforeFileKeys = Object.keys(file1);
   const difference = beforeFileKeys.reduce((acc, key) => {
     if (_.has(file2, key) && file1[key] === file2[key]) {
       return [`${key}: ${file1[key]}`, ...acc];
